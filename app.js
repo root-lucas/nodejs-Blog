bb9f33521bd53ec7a2bcf4ae2241b666b6fcfdb4
root-lucas <945832032@qq.com>
2020-01-22 22:16:01 +0800

    add: 初始化路由

---
+++ app.js
@@ -0,0 +1,32 @@
+const handleBlogRouter = require('./src/router/blog');
+const handleUserRouter = require('./src/router/user');
+
+const serverHandle = (req, res) => {
+    // 设置返回格式 JSON
+    res.setHeader('Content-type', 'application/json');
+
+    // 处理 blog 路由
+    const blogData = handleBlogRouter(req, res);
+    if (blogData) {
+        res.end(
+            JSON.stringify(blogData)
+        );
+        return
+    }
+    
+    // 处理 user路由
+    const userData = handleUserRouter(req, res);
+    if (userData) {
+        res.end(
+            JSON.stringify(userData)
+        )
+        return
+    }
+
+    // 未命中路由, 返回 404
+    res.writeHead(404, {"Content-type":"text/plain"});
+    res.write('404 Not Found\n');
+    res.end();
+} 
+
+module.exports = serverHandle;
