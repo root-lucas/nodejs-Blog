a9bb6ccee9fe7d404d5a05dedfddf3044ad6d145
root-lucas <945832032@qq.com>
2020-01-23 22:46:36 +0800

    Add: 接口-博客登录路由完成

--- src/router/user.js
+++ src/router/user.js
@@ -1,11 +1,18 @@
+const { loginCheck } = require('../controller/user');
+const { SuccessModel, ErrorModel } = require('../model/resModel');
+
 const handleUserRouter = (req, res) => {
-    const method = req.method;
+    const method = req.method; //GET POST
 
 
     // 登录
     if (method === 'POST' && req.path === '/api/user/login') {
-        return {
-            msg: '这是登录的接口'
+        const { username, password } = req.body;
+        const result = loginCheck(username, password);
+        if (result) {
+            return new SuccessModel(result);
+        } else {
+            return new ErrorModel("登录博客失败");
         }
     }
 }
