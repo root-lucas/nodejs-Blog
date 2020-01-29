820705e260276ff213f55d16bd6f36ff49f6dbd8
root-lucas <945832032@qq.com>
2020-01-29 21:34:58 +0800

    add: 登录-做cookie限制

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,13 @@
 const { login } = require('../controller/user');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
 
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+    const d = new Date();
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000)); // 24小时
+    return d.toGMTString()
+}
+
 const handleUserRouter = (req, res) => {
     const method = req.method; //GET POST
 
@@ -12,8 +19,8 @@ const handleUserRouter = (req, res) => {
         const result = login(username, password);
         return result.then(data => {
             if (data.username) {
-                // 操作 cookie
-                res.setHeader('Set-Cookie', `username=${data.username}; path=/`); // 不写path的话默认是api/user/login生效
+                // 操作 cookie, httpOnly属性是限制客户端js脚本获取该条cookie信息, 防止xss攻击
+                res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`); // 不写path的话默认是api/user/login生效
                 return new SuccessModel();
             }
             return new ErrorModel("登录失败");
