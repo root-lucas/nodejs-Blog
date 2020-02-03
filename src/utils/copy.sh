3a5d973d79332aa610661b15f848b9c1deb8cf08
root-lucas <945832032@qq.com>
2020-02-04 02:51:27 +0800

    add: 日志-写入拆分和分析

---
+++ src/utils/copy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# 拆分日志
+# linux 使用
+# cd home/lucas/blog/logs
+# cp access.log $(date +%Y-%m-%d).access.log
+# echo "" > home/lucas/blog/logs/access.log
+
+# window 使用 
+
+cp F:\\All-Project\\test\\nodeJS-Blog\\logs\\access.log $(date +%Y-%m-%d).access.log
+
+# echo "" > F:\\All-Project\\test\\nodeJS-Blog\\logs\\access.log
---
+++ src/utils/log.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+// 写日志
+function writeLog(writeStream, log) {
+    writeStream.write(log + '\n');  // 关键代码
+}
+
+// 生成 write Stream
+function createWriteStream(fileName) {
+    const fullFileName = path.join(__dirname, '../', '../', 'logs', fileName);
+    const writeStream = fs.createWriteStream(fullFileName, {flags: 'a'});
+    return writeStream;
+}
+
+// 写访问日志
+const accessWrieStream = createWriteStream('access.log');
+function access(log) {
+    writeLog(accessWrieStream, log);
+}
+
+module.exports = {
+    access
+}
